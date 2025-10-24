ee54258838c9ff42328f60df66abbd4480257f87
Yunxiang Yao <egghead_yao@sjtu.edu.cn>
2025-10-24 19:56:03 +0800

    feat: SelectQuery.h

---
+++ src/query/data/SelectQuery.h
@@ -0,0 +1,18 @@
+#ifndef PROJECT_SELECTQUERY_H
+#define PROJECT_SELECTQUERY_H
+
+#include "../Query.h"
+
+class SelectQuery : public ComplexQuery {
+  static constexpr const char *qname = "SELECT";
+  std::vector<Table::FieldIndex> fieldId;
+
+public:
+  using ComplexQuery::ComplexQuery;
+
+  QueryResult::Ptr execute() override;
+
+  std::string toString() override;
+};
+
+#endif // PROJECT_UPDATEQUERY_H
