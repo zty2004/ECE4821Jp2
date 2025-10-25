0f6e96a5b82240999e13af86f3bffc48dff6048f
Zuo Tianyou <fztym@sjtu.edu.cn>
2025-10-25 20:18:59 +0800

    feat: duplicate query imple

--- src/query/data/DuplicateQuery.cpp
+++ src/query/data/DuplicateQuery.cpp
@@ -5,6 +5,7 @@
 #include "DuplicateQuery.h"
 
 #include <algorithm>
+#include <memory>
 
 #include "../../db/Database.h"
 #include "../QueryResult.h"
@@ -18,11 +19,29 @@ QueryResult::Ptr DuplicateQuery::execute() {
         qname, this->targetTable.c_str(),
         "Invalid number of operands (? operands)."_f % operands.size());
   Database &db = Database::getInstance();
+  Table::SizeType counter = 0;
   try {
     auto &table = db[this->targetTable];
     auto result = initCondition(table);
     if (result.second) {
+      for (auto it = table.begin(); it != table.end(); ++it) {
+        if (this->evalCondition(*it)) {
+          Table::KeyType key = it->key();
+          string copyKey = key + "_copy";
+          // if cannot find existed key, avoid copy again!
+          if (!table[copyKey]) {
+            vector<Table::ValueType> copyData;
+            for (auto i = 0; i < table.field().size(); ++i) {
+              copyData.push_back((*it)[i]);
+            }
+            table.insertByIndex(key, std::move(copyData));
+            ++counter;
+          }
+        }
+        ++counter;
+      }
     }
+    return make_unique<RecordCountResult>(counter);
   } catch (const TableNameNotFound &e) {
     return make_unique<ErrorMsgResult>(qname, this->targetTable,
                                        "No such table."s);
