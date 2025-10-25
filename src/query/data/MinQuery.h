e4aac8d020bfcdbbbc2aa377d6d36318b23be27f
YUcxovo <junglcuxo@sjtu.edu.cn>
2025-10-25 20:31:05 +0800

    feat: MinQuery.h

---
+++ src/query/data/MinQuery.h
@@ -0,0 +1,20 @@
+#ifndef PROJECT_MINQUERY_H
+#define PROJECT_MINQUERY_H
+
+#include "../Query.h"
+#include "../QueryResult.h"
+
+class MinQuery : public ComplexQuery {
+  static constexpr const char *qname = "MIN";
+
+  std::vector<Table::FieldIndex> fieldId;
+
+public:
+  using ComplexQuery::ComplexQuery;
+
+  QueryResult::Ptr execute() override;
+
+  std::string toString() override;
+};
+
+#endif // PROJECT_MINQUERY_H
