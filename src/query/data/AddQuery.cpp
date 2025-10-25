7aee71118b4d93614bc4c6c805e1ab68833996d6
YUcxovo <junglcuxo@sjtu.edu.cn>
2025-10-25 22:41:02 +0800

    feat: AddQuery.cpp

--- src/query/data/AddQuery.cpp
+++ src/query/data/AddQuery.cpp
@@ -1,7 +1,59 @@
 #include "AddQuery.h"
+#include "../../db/Database.h"
+
+constexpr const char *AddQuery::qname;
 
 QueryResult::Ptr AddQuery::execute() {
-  return std::make_unique<NullQueryResult>();
+  using namespace std;
+
+  // Expect at least dst + one src
+  if (this->operands.size() < 2) {
+    return make_unique<ErrorMsgResult>(
+        qname, this->targetTable.c_str(),
+        "Invalid number of operands (? operands)."_f % operands.size());
+  }
+
+  try {
+    auto &db = Database::getInstance();
+    auto &table = db[this->targetTable];
+
+    // Resolve indices
+    dstId = table.getFieldIndex(this->operands[0]);
+    srcId.clear();
+    srcId.reserve(this->operands.size() - 1);
+    for (size_t i = 1; i < this->operands.size(); ++i) {
+      srcId.emplace_back(table.getFieldIndex(this->operands[i]));
+    }
+
+    auto condInit = initCondition(table);
+
+    // Iterate and update
+    size_t counter = 0;
+    if (condInit.second) {
+      for (auto it = table.begin(); it != table.end(); ++it) {
+        if (evalCondition(*it)) {
+          int delta = 0;
+          for (auto idx : srcId) {
+            delta += (*it)[idx];
+          }
+          (*it)[dstId] += delta;
+          ++counter;
+        }
+      }
+    }
+
+    return make_unique<RecordCountResult>(static_cast<int>(counter));
+  } catch (const TableNameNotFound &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "No such table."s);
+  } catch (const IllFormedQueryCondition &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+  } catch (const std::exception &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Unkonwn error '?'."_f % e.what());
+  }
 }
 
-std::string AddQuery::toString() { return "QUERY = ADD"; }
+std::string AddQuery::toString() {
+  return "QUERY = ADD " + this->targetTable + "\"";
+}
