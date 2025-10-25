9c2d8426e360c9be249da59e8650999d28dd8e95
Zuo Tianyou <fztym@sjtu.edu.cn>
2025-10-25 19:36:03 +0800

    feat: duplicate err msg

--- src/query/data/DuplicateQuery.cpp
+++ src/query/data/DuplicateQuery.cpp
@@ -11,8 +11,29 @@
 
 constexpr const char *DuplicateQuery::qname;
 
-QueryResult::Ptr DuplicateQuery::execute() {}
+QueryResult::Ptr DuplicateQuery::execute() {
+  using namespace std;
+  if (!this->operands.empty())
+    return make_unique<ErrorMsgResult>(
+        qname, this->targetTable.c_str(),
+        "Invalid number of operands (? operands)."_f % operands.size());
+  Database &db = Database::getInstance();
+  try {
+    auto &table = db[this->targetTable];
+    auto result = initCondition(table);
+    if (result.second) {
+    }
+  } catch (const TableNameNotFound &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "No such table."s);
+  } catch (const IllFormedQueryCondition &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+  } catch (const exception &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Unkonwn error '?'."_f % e.what());
+  }
+}
 
-std::string DuplciateQuery::toString() {
+std::string DuplicateQuery::toString() {
   return "QUERY = Duplicate " + this->targetTable + "\"";
 }
