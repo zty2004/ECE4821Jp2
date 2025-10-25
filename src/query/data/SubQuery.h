a5dc357a34a3c33920b01be73238485c7d621d59
YUcxovo <junglcuxo@sjtu.edu.cn>
2025-10-25 22:16:19 +0800

    feat: SubQuery.h

---
+++ src/query/data/SubQuery.h
@@ -0,0 +1,22 @@
+#ifndef PROJECT_SUBQUERY_H
+#define PROJECT_SUBQUERY_H
+
+#include "../Query.h"
+#include "../QueryResult.h"
+
+class SubQuery : public ComplexQuery {
+  static constexpr const char *qname = "SUB";
+
+  // Destination field and source fields to subtract
+  Table::FieldIndex dstId;
+  std::vector<Table::FieldIndex> srcId;
+
+public:
+  using ComplexQuery::ComplexQuery;
+
+  QueryResult::Ptr execute() override;
+
+  std::string toString() override;
+};
+
+#endif // PROJECT_SUBQUERY_H
