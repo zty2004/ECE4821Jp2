17a7defbe281b1dcb08f4d136ff38c814ee6688f
Yunxiang Yao <egghead_yao@sjtu.edu.cn>
2025-10-26 20:48:22 +0800

    ci: code quality tools

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ls
-exit(1)
+cd "$(dirname "$0")/.."
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_C_COMPILER=/usr/bin/clang-18 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-18 ..
+cmake --build . -- -j8
