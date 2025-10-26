f4c6b04bb13f90547a197bb3896d19ea7afce6b2
Yunxiang Yao <egghead_yao@sjtu.edu.cn>
2025-10-27 00:27:09 +0800

    ci: check code quality in run on push [test]

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
+
+echo "[COMPILE]"
+
 rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_C_COMPILER=/usr/bin/clang-18 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-18 ..
 cmake --build . -- -j8
+
+echo "[CLANG-TIDY]"
+tools/cq-clang-tidy
+
+echo "[CLANG-TIDY]"
+tools/cq-clang-tidy
+
+echo "[CPPCHECK]"
+tools/cq-cppcheck
+
+echo "[CPPLINT]"
+tools/cq-cpplint
