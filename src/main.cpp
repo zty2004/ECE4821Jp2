f896a498768dec2d09b7dfe450165f13da52e249
Yunxiang Yao <egghead_yao@sjtu.edu.cn>
2025-11-04 11:33:56 +0800

    fix: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,54 +2,67 @@
 // Created by liu on 18-10-21.
 //
 
-#include <getopt.h>
-
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
+#include <exception>
 #include <fstream>
+#include <getopt.h> // NOLINT(misc-include-cleaner)
 #include <iostream>
+#include <memory>
 #include <string>
 
+#include "query/Query.h"
 #include "query/QueryBuilders.h"
 #include "query/QueryParser.h"
+#include "query/QueryResult.h"
 
-struct {
+namespace {
+struct ParsedArgs {
   std::string listen;
   int64_t threads = 0;
-} parsedArgs;
+} parsedArgs; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+} // anonymous namespace
 
-void parseArgs(int argc, char *argv[]) {
-  const option longOpts[] = {{"listen", required_argument, nullptr, 'l'},
-                             {"threads", required_argument, nullptr, 't'},
-                             {nullptr, no_argument, nullptr, 0}};
+void parseArgs(int argc, char *argv[]) { // NOLINT(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+  constexpr int base_ten = 10;
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays,misc-include-cleaner)
+  const option longOpts[] = {{"listen", required_argument, nullptr, 'l'}, // NOLINT(misc-include-cleaner)
+                             {"threads", required_argument, nullptr, 't'}, // NOLINT(misc-include-cleaner)
+                             {nullptr, no_argument, nullptr, 0}}; // NOLINT(misc-include-cleaner)
   const char *shortOpts = "l:t:";
-  int opt, longIndex;
+  int opt = 0;
+  int longIndex = 0;
+  // NOLINTNEXTLINE(misc-include-cleaner,cppcoreguidelines-pro-bounds-array-to-pointer-decay)
   while ((opt = getopt_long(argc, argv, shortOpts, longOpts, &longIndex)) !=
          -1) {
     if (opt == 'l') {
-      parsedArgs.listen = optarg;
+      parsedArgs.listen = optarg; // NOLINT(misc-include-cleaner)
     } else if (opt == 't') {
-      parsedArgs.threads = std::strtoll(optarg, nullptr, 10);
+      parsedArgs.threads = std::strtoll(optarg, nullptr, base_ten); // NOLINT(misc-include-cleaner)
     } else {
       std::cerr << "lemondb: warning: unknown argument "
-                << longOpts[longIndex].name << std::endl;
+                // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-constant-array-index)
+                << longOpts[longIndex].name << '\n';
     }
   }
 }
 
-std::string extractQueryString(std::istream &is) {
+auto extractQueryString(std::istream &input_stream) -> std::string {
   std::string buf;
-  do {
-    int const ch = is.get();
-    if (ch == ';')
+  while (true) {
+    int const char_read = input_stream.get();
+    if (char_read == ';') {
       return buf;
-    if (ch == EOF)
+    }
+    if (char_read == EOF) {
       throw std::ios_base::failure("End of input");
-    buf.push_back(static_cast<char>(ch));
-  } while (true);
+    }
+    buf.push_back(static_cast<char>(char_read));
+  }
 }
 
-int main(int argc, char *argv[]) {
+auto main(int argc, char *argv[]) -> int {
   // Assume only C++ style I/O is used in lemondb
   // Do not use printf/fprintf in <cstdio> with this line
   std::ios_base::sync_with_stdio(false);
@@ -61,18 +74,18 @@ int main(int argc, char *argv[]) {
     fin.open(parsedArgs.listen);
     if (!fin.is_open()) {
       std::cerr << "lemondb: error: " << parsedArgs.listen
-                << ": no such file or directory" << std::endl;
+                << ": no such file or directory" << '\n';
       exit(-1);
     }
   }
-  std::istream is(fin.rdbuf());
+  std::istream input_stream(fin.rdbuf());
 
 #ifdef NDEBUG
   // In production mode, listen argument must be defined
   if (parsedArgs.listen.empty()) {
     std::cerr << "lemondb: error: --listen argument not found, not allowed in "
                  "production mode"
-              << std::endl;
+              << '\n';
     exit(-1);
   }
 #else
@@ -80,37 +93,37 @@ int main(int argc, char *argv[]) {
   if (parsedArgs.listen.empty()) {
     std::cerr << "lemondb: warning: --listen argument not found, use stdin "
                  "instead in debug mode"
-              << std::endl;
-    is.rdbuf(std::cin.rdbuf());
+              << '\n';
+    input_stream.rdbuf(std::cin.rdbuf());
   }
 #endif
 
   if (parsedArgs.threads < 0) {
     std::cerr << "lemondb: error: threads num can not be negative value "
-              << parsedArgs.threads << std::endl;
+              << parsedArgs.threads << '\n';
     exit(-1);
   } else if (parsedArgs.threads == 0) {
     // @TODO Auto detect the thread num
-    std::cerr << "lemondb: info: auto detect thread num" << std::endl;
+    std::cerr << "lemondb: info: auto detect thread num" << '\n';
   } else {
     std::cerr << "lemondb: info: running in " << parsedArgs.threads
-              << " threads" << std::endl;
+              << " threads" << '\n';
   }
 
-  QueryParser p;
+  QueryParser parser;
 
-  p.registerQueryBuilder(std::make_unique<QueryBuilder(Debug)>());
-  p.registerQueryBuilder(std::make_unique<QueryBuilder(ManageTable)>());
-  p.registerQueryBuilder(std::make_unique<QueryBuilder(Complex)>());
+  parser.registerQueryBuilder(std::make_unique<QueryBuilder(Debug)>());
+  parser.registerQueryBuilder(std::make_unique<QueryBuilder(ManageTable)>());
+  parser.registerQueryBuilder(std::make_unique<QueryBuilder(Complex)>());
 
   size_t counter = 0;
 
-  while (is) {
+  while (input_stream) {
     try {
       // A very standard REPL
       // REPL: Read-Evaluate-Print-Loop
-      std::string const queryStr = extractQueryString(is);
-      Query::Ptr query = p.parseQuery(queryStr);
+      std::string const queryStr = extractQueryString(input_stream);
+      Query::Ptr query = parser.parseQuery(queryStr);
       QueryResult::Ptr result = query->execute();
       std::cout << ++counter << "\n";
       if (result->success()) {
@@ -129,9 +142,9 @@ int main(int argc, char *argv[]) {
     } catch (const std::ios_base::failure &) {
       // End of input
       break;
-    } catch (const std::exception &e) {
+    } catch (const std::exception &exception_obj) {
       std::cout.flush();
-      std::cerr << e.what() << std::endl;
+      std::cerr << exception_obj.what() << '\n';
     }
   }
 
