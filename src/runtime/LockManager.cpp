3104d4c6ae0c3a36b45dca2fd0c79504e3645127
zlt_0503 <zlt_0503@sjtu.edu.cn>
2025-11-12 18:23:23 +0800

    feat: impl for lock manager

---
+++ src/runtime/LockManager.cpp
@@ -0,0 +1,60 @@
+//
+// LockManager implementation
+//
+
+#include "LockManager.h"
+
+LockManager::Entry &LockManager::entry(const TableId &id) {
+  std::scoped_lock lk(mapMtx_);
+  auto it = map_.find(id);
+  if (it == map_.end()) {
+    it = map_.emplace(id, std::make_unique<Entry>()).first;
+  }
+  return *it->second;
+}
+
+bool LockManager::tryLockS(const TableId &id) {
+  auto &e = entry(id);
+  return e.rw.try_lock_shared();
+}
+
+void LockManager::unlockS(const TableId &id) {
+  auto &e = entry(id);
+  e.rw.unlock_shared();
+}
+
+bool LockManager::tryLockX(const TableId &id) {
+  auto &e = entry(id);
+  return e.rw.try_lock();
+}
+
+void LockManager::unlockX(const TableId &id) {
+  auto &e = entry(id);
+  e.rw.unlock();
+}
+
+void LockManager::writerIntentBegin(const TableId &id) {
+  auto &e = entry(id);
+  e.waiting_writers.fetch_add(1, std::memory_order_release);
+}
+
+void LockManager::writerIntentEnd(const TableId &id) {
+  auto &e = entry(id);
+  e.waiting_writers.fetch_sub(1, std::memory_order_release);
+}
+
+bool LockManager::canAdmitShared(const TableId &id) const noexcept {
+  const Entry *found = [&]() -> const Entry * {
+    std::scoped_lock lk(mapMtx_);
+    if (auto it = map_.find(id); it != map_.end()) {
+      return it->second.get();
+    }
+    return nullptr;
+  }();
+
+  if (!found) {
+    return true;
+  }
+
+  return found->waiting_writers.load(std::memory_order_acquire) == 0;
+}
