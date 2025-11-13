e87cab95119bd1fd4269b864d9aca5d6b4dea1ff
Yunxiang Yao <egghead_yao@sjtu.edu.cn>
2025-11-13 18:51:02 +0800

    fix: src/runtime/QueryTask.h clang-tidy

--- src/runtime/QueryTask.h
+++ src/runtime/QueryTask.h
@@ -15,25 +15,30 @@
 
 using TableId = std::string;
 
-enum class OpKind { Read, Write };
+enum class OpKind : std::uint8_t { Read, Write };
 
 struct QueryTask {
+  // NOLINTBEGIN(misc-non-private-member-variables-in-classes)
   TableId table;
   OpKind kind = OpKind::Read;
   Query::Ptr query;
   std::uint32_t attempts = 0;
   std::size_t orderIndex = 0;
+  // NOLINTEND(misc-non-private-member-variables-in-classes)
 
   QueryTask() = default;
+  ~QueryTask() = default;
 
-  QueryTask(TableId t, OpKind k, Query::Ptr q, std::size_t idx)
-      : table(std::move(t)), kind(k), query(std::move(q)), orderIndex(idx) {}
+  QueryTask(TableId tableArg, OpKind kindArg, Query::Ptr queryArg,
+            std::size_t idx)
+      : table(std::move(tableArg)), kind(kindArg), query(std::move(queryArg)),
+        orderIndex(idx) {}
 
   QueryTask(QueryTask &&) = default;
-  QueryTask &operator=(QueryTask &&) = default;
+  auto operator=(QueryTask &&) -> QueryTask & = default;
 
   QueryTask(const QueryTask &) = delete;
-  QueryTask &operator=(const QueryTask &) = delete;
+  auto operator=(const QueryTask &) -> QueryTask & = delete;
 };
 
 #endif  // SRC_RUNTIME_QUERYTASK_H_
