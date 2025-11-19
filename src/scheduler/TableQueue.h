37f5f5e641230afe7e7937808219bbcb992ca25b
YUcxovo <junglcuxo@sjtu.edu.cn>
2025-11-19 17:00:57 +0800

    feat: helper interface for TableQueue

--- src/scheduler/TableQueue.h
+++ src/scheduler/TableQueue.h
@@ -10,6 +10,9 @@ struct TableQueue {
   bool registered = false;       // LOAD or not
   std::uint64_t registerSeq = 0; // seq of LOAD
   std::deque<ScheduledItem> queue;
+
+  [[nodiscard]] auto size() const -> std::size_t { return queue.size(); }
+  [[nodiscard]] auto empty() const -> bool { return queue.empty(); }
 };
 
 #endif // SRC_SCHEDULER_TABLEQUEUE_H_
