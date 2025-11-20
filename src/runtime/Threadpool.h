6194b9f6d99f6e199843d3066db0377c7bd9e717
Zuo Tianyou <fztym@sjtu.edu.cn>
2025-11-20 22:20:30 +0800

    feat: thread pool constructor

--- src/runtime/Threadpool.h
+++ src/runtime/Threadpool.h
@@ -7,27 +7,35 @@
 #ifndef SRC_RUNTIME_THREADPOOL_H_
 #define SRC_RUNTIME_THREADPOOL_H_
 
+#define __cpp_lib_jthread
+
 #include <cstddef>
-#include <thread>
-#include <vector>
 
 #ifdef __cpp_lib_jthread
 /**
  * @brief The type of threads to use. In C++20 and later we use `std::jthread`.
  */
+#include <stop_token>
+#include <thread>
 using thread_t = std::jthread;
 #else
 /**
  * @brief The type of threads to use. In C++17 we use`std::thread`.
  */
+#include <thread>
 using thread_t = std::thread;
 #endif
 
-class Threadpool {
+template <std::size_t PoolSize> class Threadpool {
 private:
-  std::vector<thread_t> threads;
+  static constexpr std::size_t thread_count = PoolSize;
+  std::array<thread_t, PoolSize> threads;
 
 public:
+  Threadpool(const Threadpool &) = delete;
+  Threadpool(Threadpool &&) = delete;
+  auto operator=(const Threadpool &) -> Threadpool & = delete;
+  auto operator=(Threadpool &&) -> Threadpool & = delete;
   [[nodiscard]] auto get_thread_count() const -> size_t {
     return this->threads.size();
   }
