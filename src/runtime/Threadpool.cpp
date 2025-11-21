bc24afe93a78bd9073279a0a3c327b1c24bce281
Zuo Tianyou <fztym@sjtu.edu.cn>
2025-11-22 01:22:20 +0800

    chore: split Threadpool.cpp

--- src/runtime/Threadpool.cpp
+++ src/runtime/Threadpool.cpp
@@ -46,3 +46,28 @@ template <std::size_t PoolSize>
 Threadpool<PoolSize>::WriteGuard::~WriteGuard() {
   lm_.unlockX(id_);
 }
+
+template <std::size_t PoolSize>
+Threadpool<PoolSize>::ReadGuard::ReadGuard(LockManager &lkm, TableId index)
+    : lm_(lkm), id_(std::move(index)) {
+  lm_.lockS(id_);
+}
+
+template <std::size_t PoolSize> Threadpool<PoolSize>::ReadGuard::~ReadGuard() {
+  lm_.unlockS(id_);
+}
+
+#ifdef __cpp_lib_jthread
+template <std::size_t PoolSize>
+void Threadpool<PoolSize>::worker_loop(const std::stop_token &st) {
+  while (!st.stop_requested()) {
+    work();
+  }
+}
+#else
+template <std::size_t PoolSize> void Threadpool<PoolSize>::worker_loop() {
+  while (!stop_flag_.load(std::memory_order_acquire)) {
+    work();
+  }
+}
+#endif
