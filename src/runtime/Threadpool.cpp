0933c3ac2e3a9899f3ce85c0cd4c63f640ca7977
Zuo Tianyou <fztym@sjtu.edu.cn>
2025-11-22 01:21:52 +0800

    chore: split Threadpool.cpp

---
+++ src/runtime/Threadpool.cpp
@@ -0,0 +1,48 @@
+//
+// Threadpool
+// When the threadpool has empty slot, it asks scheduler for tasks. This class
+// focues on thread management and synchronization.
+//
+
+#include "Threadpool.h"
+#include "../query/QueryHelpers.h"
+
+template <std::size_t PoolSize>
+Threadpool<PoolSize>::Threadpool(LockManager &lm, TaskQueue &tq)
+    : lock_manager_(lm), task_queue_(tq) {
+  for (std::size_t i = 0; i < PoolSize; ++i) {
+#ifdef __cpp_lib_jthread
+    threads[i] =
+        thread_t([this](std::stop_token st) { this->worker_loop(st); });
+#else
+    threads[i] = thread_t([this] { this->worker_loop(); });
+#endif
+  }
+}
+
+template <std::size_t PoolSize> Threadpool<PoolSize>::~Threadpool() {
+  stop_flag_.store(true);
+#ifndef __cpp_lib_jthread
+  for (auto &t : threads) {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+#endif
+}
+
+template <std::size_t PoolSize>
+auto Threadpool<PoolSize>::get_threadpool_size() const -> size_t {
+  return thread_count;
+}
+
+template <std::size_t PoolSize>
+Threadpool<PoolSize>::WriteGuard::WriteGuard(LockManager &lkm, TableId index)
+    : lm_(lkm), id_(std::move(index)) {
+  lm_.lockX(id_);
+}
+
+template <std::size_t PoolSize>
+Threadpool<PoolSize>::WriteGuard::~WriteGuard() {
+  lm_.unlockX(id_);
+}
